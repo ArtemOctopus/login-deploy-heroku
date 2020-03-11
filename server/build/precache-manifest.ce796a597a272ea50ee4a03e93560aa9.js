4ed9a744282fb5db48947f814f24eabf4444af11
ArtemOctopus <61685260+ArtemOctopus@users.noreply.github.com>
2020-03-11 15:33:42 +0300

    Add files via upload

---
+++ server/build/precache-manifest.ce796a597a272ea50ee4a03e93560aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60709b2d0d6cc8a80e7daf34ad7500a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467202716761501d576a",
+    "url": "/static/css/main.de0ea5ed.chunk.css"
+  },
+  {
+    "revision": "2575c3a08054459ee28e",
+    "url": "/static/js/2.607f078f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.607f078f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467202716761501d576a",
+    "url": "/static/js/main.dd4ce432.chunk.js"
+  },
+  {
+    "revision": "cbf9522bdfd6b0fd3511",
+    "url": "/static/js/runtime-main.4c05f58b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
